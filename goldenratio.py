47b434aa2884b0514348edcfc437b97b4a9cb773
mkelleyjr <michael.kelley.jr@gmail.com>
2023-05-27 02:55:16 -0400

    golden ratio example

---
+++ goldenratio.py
@@ -0,0 +1,10 @@
+# Michael L. Kelley Jr.
+# May 26, 2023
+# goldenratio.py
+# example of the golden ratio in Python
+# see more at: https://www.mathsisfun.com/numbers/golden-ratio.html
+
+golden = (1+5**0.5)/2
+
+print(golden)
+
