5d6f1c58c9268a1bb422fe8ef09a08e7141526ba
mkelleyjr <michael.kelley.jr@gmail.com>
2023-06-24 18:27:58 -0400

    type() function example

---
+++ type.py
@@ -0,0 +1,23 @@
+# Michael L. Kelley Jr.
+# June 24, 2023
+# type.py
+# Checking data types with type()
+# Useful for debugging, type() will return the data type of a variable 
+
+# Define some variables to use 
+
+number = 6
+name = "Mike"
+floatNumber = 6.0 
+
+# Using type() to print the data types of variables 
+
+print (type(number))
+
+print (type(name))
+
+print (type(floatNumber))
+
+
+
+
