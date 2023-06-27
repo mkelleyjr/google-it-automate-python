5e69af44e22d0c4bcac09295b52c4698ad066e5f
mkelleyjr <michael.kelley.jr@gmail.com>
2023-06-26 20:19:14 -0400

    added product.py file

---
+++ product.py
@@ -0,0 +1,11 @@
+# Michael L. Kelley Jr.
+# June 26, 2023
+# product.py
+# Print the product of all numbers in a range
+
+product = 1
+for n in range (1, 20):
+    product = product * n
+
+print(product)
+
