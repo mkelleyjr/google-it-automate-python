08d7ebb4ddc28e7d3f8b1b1cbf49aea870d1a86a
mkelleyjr <michael.kelley.jr@gmail.com>
2023-06-26 20:27:53 -0400

    showing possible team matchups from a list

---
+++ team-matchups.py
@@ -0,0 +1,14 @@
+# Michael L. Kelley Jr.
+# June 26, 2023
+# team-matchups.py
+# Print all of the possible matchups between teams
+# Take into consideration home + away status
+
+teams = ['Dragons', 'Bears', 'Pandas', 'Unicorns']
+
+for home_team in teams:
+    for away_team in teams:
+        if home_team != away_team:
+            print(home_team + " vs " + away_team)
+            
+            
