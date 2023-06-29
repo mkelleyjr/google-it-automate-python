250683f430c5f39418305d237a23925104659847
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2023-06-29 10:33:51 -0400

    updated folder structure and added files from notepad++

---
+++ Module1/binary-number.py
@@ -0,0 +1,9 @@
+# Michael L. Kelley Jr.
+# June 27, 2023
+# binary-number.py
+# Returns the binary value of a given number
+
+
+number = 21
+print(bin(number))
+
---
+++ Module1/string-format.py
@@ -0,0 +1,13 @@
+# Michael L. Kelley Jr.
+# June 27, 2023
+# string-format.py
+# Formatting strings
+
+
+name = "Michael"
+number = len(name) * 3
+
+# Use placeholder {} for the variables 
+print("Hello {}, your lucky number is {}".format(name, number))
+
+
